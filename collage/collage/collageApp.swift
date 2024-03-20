45557d923fa0b1f7e24370e396d9f04f6116d160
maliakuo <70962881+maliakuo@users.noreply.github.com>
2024-03-20 07:52:05 -1000

    first commit

---
+++ collage/collage/collageApp.swift
@@ -0,0 +1,32 @@
+//
+//  collageApp.swift
+//  collage
+//
+//  Created by Malia Kuo on 3/20/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct collageApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
